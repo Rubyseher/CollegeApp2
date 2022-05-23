7d09f77a8f1f930c7fb7b6e73c2a36ef0dae3c70
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2022-05-23 21:09:29 +0530

    Revert "fixed login page"
    This reverts commit 30f150a170dfdd670799a5b09c3f5d6956e3d3c4.

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -7,9 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class
 LoginActivity extends AppCompatActivity {
     Button btn;
@@ -24,7 +21,6 @@ LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this,admin_home_page.class);
                 startActivity(intent);
-
 
             }
 
--- app/src/main/java/com/example/myapplication/LoginActivity3.java
+++ app/src/main/java/com/example/myapplication/LoginActivity3.java
@@ -1,78 +1,26 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.annotation.NonNull;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.example.myapplication.authentication.RegisterActivity;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.example.myapplication.R;
-import com.google.android.gms.tasks.Task;
 
 public class
 LoginActivity3 extends AppCompatActivity {
     Button btn;
 
-    private EditText  mEmail, mPass;
-    private FirebaseDatabase db=FirebaseDatabase.getInstance();
-    private DatabaseReference root =db.getReference();
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login3);
-
-        mEmail = findViewById(R.id.user_email);
-        mPass = findViewById(R.id.user_pass);
         btn = (Button)findViewById(R.id.button123);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intent = new Intent(LoginActivity3.this,student_page.class);
-//                startActivity(intent);
-                String email=mEmail.getText().toString();
-                String pass=mPass.getText().toString();
-
-                Log.d("email",email);
-                root.child(email).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DataSnapshot> task) {
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(LoginActivity3.this, "Error", Toast.LENGTH_SHORT).show();
-                        }
-                        else {
-                            Toast.makeText(LoginActivity3.this, "Read Success", Toast.LENGTH_SHORT).show();
-                            for (DataSnapshot ds : task.getResult().getChildren()) {
-                                String key = ds.getKey();
-                                String value = ds.getValue(String.class);
-                                Log.d("TAG", key + ":" + value);
-                                if(key == "pass" && value == pass) {
-                                    Toast.makeText(LoginActivity3.this, "Login Success", Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(LoginActivity3.this, "Invalid Username/pass", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-
-                        }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(LoginActivity3.this, "Error", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                Intent intent = new Intent(LoginActivity3.this,student_page.class);
+                startActivity(intent);
 
             }
 
